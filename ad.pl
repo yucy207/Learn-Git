346c8e7332403eb61ff1d923d22fbd2abd1203b6
yucy207 <52816230+yucy207@users.noreply.github.com>
2019-07-25 14:40:10 +0800

    asd

--- ad.pl
+++ ad.pl
@@ -4,8 +4,4 @@ use warnings;
 my %hash;
 my $IN = shift @ARGV;
 my $OUT = shift @ARGV;
-open IN , $IN or die "can't open $IN!\n";
-open OUT, ">$OUT" or die "can't open $OUT!\n";
-my $head= <IN>;
-sadasdasd
-asasc
+open IN , $IN or die "can't open $IN!\n
