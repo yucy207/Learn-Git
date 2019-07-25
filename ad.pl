36e7812fec3cddb97681b87c04fcd2fc7faee97e
yucy207 <52816230+yucy207@users.noreply.github.com>
2019-07-25 11:01:02 +0800

    asd

--- ad.pl
+++ ad.pl
@@ -18,10 +18,3 @@ while (<IN>){
     map {$hash{$heads[$_]}{$.-1}=exists $line[$_]?$line[$_]:""} 0..$#heads;
     # map {$hash{$heads[$_]}=$line[$_]} 0..$#line;
     map {print OUT "$_\t"} @heads;
-    print OUT "\n";
-    map {print OUT "$hash{$_}{$.-1}\t"} @heads;
-    print OUT "\n";
-}
-close IN;
-close OUT;
-print "$hash{'Position'}{'2'}";asdasdad
